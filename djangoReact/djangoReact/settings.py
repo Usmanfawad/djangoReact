35fc82e54ece878b82ea401baf2018d8d2d141c3
Usman Fawad <ufawad0@gmail.com>
2022-03-15 04:42:01 +0500

    Done with models API

--- djangoReact/djangoReact/settings.py
+++ djangoReact/djangoReact/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'reactApp.apps.ReactappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- djangoReact/reactApp/models.py
+++ djangoReact/reactApp/models.py
@@ -1,9 +1,19 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return "[{},{}]".format(self.question_text,self.pub_date)
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
     #CASCADE: When the referenced object is deleted, also delete the objects that have references to it 
@@ -11,3 +21,6 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return "[{},{}]".format(self.choice_text,self.votes)
