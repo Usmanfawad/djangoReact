f08dc28b6d8e9471889bb1f86e70f96f30260bf9
Usman Fawad <ufawad0@gmail.com>
2022-03-15 03:21:55 +0500

    added models

--- djangoReact/reactApp/models.py
+++ djangoReact/reactApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+    #CASCADE: When the referenced object is deleted, also delete the objects that have references to it 
+    # (when you remove a blog post for instance, you might want to delete comments as well). SQL equivalent: CASCADE.
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
